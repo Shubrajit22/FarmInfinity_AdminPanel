f46aea4b7490519b3621ff8b929976bd714ce6b4
Shubrajit <shubrajitdeb180603@gmail.com>
2025-04-15 15:00:46 +0530

    keycloak.ts file url changed

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,9 +1,10 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'https://keycloak.farmeasytechnologies.com/',  
+  url: 'https://keycloak.farmeasytechnologies.com/auth',  
   realm: 'farm-infinity-realm-dev',           
   clientId: '001',        
 });
 
 export default keycloak;
+``
